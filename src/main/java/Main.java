f6ea7691f6c6c07c37b6f718f70f6b5ca30cff77
Caroline Rives <car.rives@gmail.com>
2022-06-24 00:34:51 -0400

    Reference Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,38 @@ public class Main {
     public static void main(String[] args) {
         NumberContainer numContainer = new NumberContainer();
         multiply(3, 5, numContainer);
+        System.out.println("Result is " + numContainer.num);
 
-// Modify the method above so that the calling method cannot see the result of the multiplication
-// Write a method that can modify a String that is passed in, in such a way that the calling method 
-// can see the modified new value for the String parameter
-// Write a method that takes a String parameter, modifies it and shows that the calling method 
-// cannot see the modified value
+        StringContainer strContainer = new StringContainer();
+        strContainer.stringy = "Hello";
+        modifyString(strContainer, ". How are you?");
+        System.out.println("String is " + strContainer.stringy);
     }
 
+//1) Write a method that multiplies two numbers and stores the result in a variable that was passed in 
+//    as a parameter. Make the calling method display the result of the multiplication.
+    // public static void multiply(int int1, int int2, NumberContainer numCon) {
+    //    numCon.num = int1 * int2;
+    //    System.out.println("Result is " + numCon.num);
+    // }
 
-// Write a method that multiplies two numbers and stores the result in a variable that was passed in 
-// as a parameter. Make the calling method display the result of the multiplication.
-    public static void multiply(int int1, int int2, NumberContainer numCon) {
-       numCon.num = int1 * int2;
-       System.out.println("Result is " + numCon.num);
-    }
+//2) Modify the method above so that the calling method cannot see the result of the multiplication
+public static void multiply(int int1, int int2, NumberContainer numCon) {
+    numCon = new NumberContainer();
+    numCon.num = int1 * int2;
+ }
+
+ //3) Write a method that can modify a String that is passed in, in such a way that the calling method 
+ //   can see the modified new value for the String parameter
+// public static void modifyString(StringContainer str, String addition) {
+//     str.stringy = str.stringy + addition;
+// }
+
+//4) Write a method that takes a String parameter, modifies it and shows that the calling method 
+//   cannot see the modified value
+public static void modifyString(StringContainer str, String addition) {
+    str = new StringContainer();
+    str.stringy = str.stringy + addition;
+}
 }
 
